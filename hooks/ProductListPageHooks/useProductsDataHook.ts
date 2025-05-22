bade461b7b543917f83c82e08cfb1e468e46539b
amir8848Digital <amir@8848Digital.com>
2025-05-22 15:42:26 +0530

    fix: change in data structure of banner hook and useProductData hook for maping api reponse

--- hooks/ProductListPageHooks/useProductsDataHook.ts
+++ hooks/ProductListPageHooks/useProductsDataHook.ts
@@ -60,30 +60,34 @@ const useProductListing = () => {
       setToggleProductListView('grid-view');
     }
   };
-  const fetchProductListDataAPI = async (reqParams: any) => {
+
+   const fetchProductListDataAPI = async (reqParams: any) => {
     let productListDataAPI: any;
     setIsLoading(true);
     try {
       productListDataAPI = await fetchProductListingFromAPI(SUMMIT_APP_CONFIG, reqParams, TokenFromStore?.token);
-      if (productListDataAPI?.data?.msg === 'success' && productListDataAPI?.data?.data?.length > 0) {
+      if (productListDataAPI?.data?.message?.msg === 'success' && productListDataAPI?.data?.message?.data?.length > 0) {
         if (CONSTANTS.SHOW_MORE_ITEMS === 'load-more') {
-          setProductListingData((prevData: any) => [...prevData, ...productListDataAPI?.data?.data]);
+          setProductListingData((prevData: any) => [...prevData, ...productListDataAPI?.data?.message?.data]);
         } else if (CONSTANTS.SHOW_MORE_ITEMS === 'paginate') {
-          setProductListingData(productListDataAPI?.data?.data);
+          setProductListingData(productListDataAPI?.data?.message?.data);
         }
-        setProductListTotalCount(productListDataAPI?.data?.total_count);
+        setProductListTotalCount(productListDataAPI?.data?.message?.total_count);
       } else {
         setProductListingData([]);
         setProductListTotalCount(0);
-        setErrMessage(productListDataAPI?.data?.error || 'An unknown error occured.');
+        setErrMessage(productListDataAPI?.data?.message.error || 'An unknown error occured.');
       }
     } catch (error) {
       setProductListingData([]);
-      setErrMessage(productListDataAPI?.data?.error || 'An unknown error occured.');
+      setErrMessage(productListDataAPI?.data?.message.error || 'An unknown error occured.');
     } finally {
       setIsLoading(false);
     }
   };
+
+
+
 
   useEffect(() => {
     const catalogSlug = router.route.split('/')[1];
