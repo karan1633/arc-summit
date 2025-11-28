e53d213702ff23f89f5a92fe52f1b53be6a027e5
8848Digital-Rohit-Frontend <rohit.g@8848digital.com>
2025-11-28 13:34:23 +0530

    feat: order details status btns and popup changes :rg

--- services/api/order-detail-apis/order-update-api.ts
+++ services/api/order-detail-apis/order-update-api.ts
@@ -15,8 +15,25 @@ export const getUserPermissionsAPI = async (appConfig: APP_CONFIG, user: any, to
   return response;
 };
 
+export const getUserPermissionsForButtonsAPI = async (appConfig: APP_CONFIG, user: any, token: any) => {
+  let additionalParams = { ...(user && { user }) };
+  // Use executeGETAPI to handle GET Request logic
+  const response = await executeGETAPI(
+    appConfig,
+    'get_user_permissions_for_buttons',
+    token,
+    additionalParams // Pass additional parameters if needed
+  );
+  return response;
+};
+
 export const readyToDispatchApi: any = async (appConfig: APP_CONFIG, apiBody: any, token: any) => {
   const response = await executePOSTAPI(appConfig, 'ready-to-dispatch-api', apiBody, token);
+  return response;
+};
+
+export const updateOrderItemStatusApi: any = async (appConfig: APP_CONFIG, apiBody: any, token: any) => {
+  const response = await executePOSTAPI(appConfig, 'update_sales_order_item_manufacturing_status_details', apiBody, token);
   return response;
 };
 
