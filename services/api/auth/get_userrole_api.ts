3516585965e3789539622c0620c9a66a45c66118
prabhudev797 <prabhudesai007@gmail.com>
2026-01-15 11:42:29 +0530

    feat: enhance UserRoleFetch to store navbar permissions in localStorage

--- services/api/auth/get_userrole_api.ts
+++ services/api/auth/get_userrole_api.ts
@@ -3,13 +3,13 @@ import { CONSTANTS } from '../../config/app-config';
 import APP_CONFIG from '../../../interfaces/app-config-interface';
 
 const UserRoleFetch = async (appConfig: APP_CONFIG, token: any) => {
-  const {access_token, unique_key} = token
+  const { access_token, unique_key } = token
   let response: any;
   const version = appConfig.version;
   const method = 'get_user_profile';
   const entity = 'signin';
   const apiSDKName = appConfig.app_name;
- 
+
   const config = {
     headers: {
       Authorization: access_token,
@@ -24,10 +24,16 @@ const UserRoleFetch = async (appConfig: APP_CONFIG, token: any) => {
     )
     .then((res: any) => {
       response = res?.data?.message?.data;
+      const navbarPermissions = JSON.stringify({
+        "is_show_dashboard": response?.is_show_dashboard,
+        "is_show_orders": response?.is_show_orders,
+        "is_show_reports": response?.is_show_reports
+      });
 
       if (Object.keys(response)?.length > 0) {
         localStorage.setItem('isDealer', response.is_dealer);
         localStorage.setItem('isSuperAdmin', response.is_superadmin);
+        localStorage.setItem('navbarPermissions', navbarPermissions);
       }
     })
     .catch((err: any) => {
